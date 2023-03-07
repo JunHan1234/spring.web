21bca327dd405b6da94fb69b97864b03abcdfc8e
'Han1234' <'a@a.com'>
2023-03-07 15:11:13 +0900

    1

--- src/main/java/com/my/spring/web/AppConfig.java
+++ src/main/java/com/my/spring/web/AppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AppConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		//request url이 context pass라면, viewname을 ch01/main으로 하겠다를 설정.
 		registry.addViewController("/").setViewName("ch01/main");
 	}
 }
--- src/main/webapp/WEB-INF/view/ch02/ex01/user.jsp
+++ src/main/webapp/WEB-INF/view/ch02/ex01/user.jsp
@@ -1,3 +1,3 @@
 <%@ page language='java' contentType='text/html; charset=utf-8' pageEncoding='utf-8' %>
-<!-- getUserName과 getUserAge를 call한다. --> 
+<!-- getUserName과 getUserAge를 el로 call한다. --> 
 ${user.username} / ${user.age}
