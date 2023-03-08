f66588499abce6de60f43014b2ec2387806847fc
'Han1234' <'a@a.com'>
2023-03-08 13:49:26 +0900

    1

--- src/main/java/com/my/spring/web/ch02/ex03/UserController.java
+++ src/main/java/com/my/spring/web/ch02/ex03/UserController.java
@@ -1,22 +1,24 @@
 package com.my.spring.web.ch02.ex03;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("ch02.ex03")
 @RequestMapping("ch02/ex03/user")
 public class UserController {
 	//이 핸들러의 url은 get방식의 ch02/ex03/user이다.
-	/*//핸들러의 url을 다른 방식(ch01)으로 구현해보았다.
+	//핸들러의 url을 다른 방식(ch01)으로 구현해보았다.
 	@GetMapping
 	public String userIn() {
 		//form을 return
 		return "ch02/ex03/userIn";
 	}
-	*/
+	
 	
 	//이 핸들러의 url은 post방식의 ch02/ex03/user이다.
-	//@PostMapping
+	@PostMapping	//command: model의 역할, request url을 보내는 역할 두가지 수행하는 것.
 	public String userOut(User user) {
 		return "ch02/ex03/userOut";
 	}
